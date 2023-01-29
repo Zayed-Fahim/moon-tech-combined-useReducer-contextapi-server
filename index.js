aefd7ef4a6a969f8adc25fa731973c9f259eff3b
Zayed Fahim <zayedfahim33.cse@gmail.com>
2023-01-29 20:53:57 +0600

    create product link and connect with mongodb

---
+++ index.js
@@ -0,0 +1,38 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@redux-projects.jetmlhc.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  const productsCollection = client.db("moon-tech").collection("products");
+  try {
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.error(error));
+
+app.get("/", (req, res) => {
+  res.send("Server is Running");
+});
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
